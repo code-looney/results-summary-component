b15049fc8b865bdc8d8e1883f56b0552da7323c2
code-looney <danielvbofficieel@gmail.com>
2024-04-05 12:09:48 +0200

    card primary desk

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,14 +9,17 @@ const Cards = () => {
   const [result, setResult] = useState(76)
 
   return (
-    <Container className={`bg-white flex w-full h-screen`}>
-        <Card className={`h-auto flex flex-col`}>
-                <Container className={`pt-6 pb-10 bg-gradient-to-t from-royal to-slate flex flex-col items-center rounded-b-[30px] gap-5`}>
-                    <Title className={`text-lavender`}>Your Result</Title>
-                    <Container className={`bg-gradient-to-b from-violet to-persian text-pale w-40 h-40 flex flex-col items-center justify-center rounded-full text-[64px] font-medium leading-snug`}>{result} <span className={`text-lavender text-[14px]`}>of 100</span></Container>
-                    <Container className={`flex flex-col items-center gap-2`}>
-                      <Sub className={`text-pale text-[28px]`}>Great</Sub>
-                      <Paragraph className={`text-lavender`}>You scored higher than 65% of the <br /> people who have taken these tests.</Paragraph>
+    <Container className={`bg-white flex w-full h-screen md:h-[500px]`}>
+        <Card className={`h-auto flex flex-col md:flex-row`}>
+                <Container className={`md:w-[400px] pt-6 pb-10 bg-gradient-to-t from-royal to-slate flex flex-col items-center rounded-b-[30px] md:rounded-[30px] gap-5 md:justify-center`}>
+                    <Title className={`text-lavender text-[21px] md:text-[24px] font-medium`}>Your Result</Title>
+                    <Container 
+                    className={`bg-gradient-to-b from-violet to-persian text-pale w-40 h-40 md:w-[220px] md:h-[220px] flex flex-col items-center justify-center rounded-full text-[64px] font-bold leading-snug`}>
+                      {result} <span 
+                    className={`text-lavender text-[14px]`}>of 100</span></Container>
+                    <Container className={`flex flex-col items-center gap-2 md:text-center`}>
+                      <Sub className={`text-pale text-[28px] font-bold`}>Great</Sub>
+                      <Paragraph className={`text-lavender`}>You scored higher than 65% of <br className={`md:block hidden`} /> the <br className={`md:hidden`} /> people who have taken <br className={`md:block hidden`} /> these tests.</Paragraph>
                     </Container>
                 </Container>
             <Card className={`bg-white flex flex-col items-center`}>
