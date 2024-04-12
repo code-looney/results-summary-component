2370301fe3cc47c02ef1daa0121f90f9c12725c8
code-looney <danielvbofficieel@gmail.com>
2024-04-12 19:13:09 +0200

    working on the li

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -30,7 +30,9 @@ const Cards = () => {
         return 'green-teal'
       } else if (index === 4) {
         return 'cobalt'
-      }
+      }else if (index === 0) {
+      return 'hidden'
+    } 
   }
 
   function handleIcons (index) {
@@ -42,13 +44,13 @@ const Cards = () => {
       return 'verbal'
     } else if(index === 4) {
       return 'visual'
-    }  
+    }
   } 
 
   return (
-    <Container className={`bg-white flex h-screen md:h-[500px] outline `}>
-        <Card className={`h-auto flex flex-col md:flex-row  w-screen md:w-auto `}>
-                <Container className={`md:w-[400px] pt-6 pb-10 bg-gradient-to-t from-royal to-slate flex flex-col items-center rounded-b-[30px] md:rounded-[30px] gap-5 md:justify-center`}>
+    <Container className={`bg-white h-screen w-screen md:w-auto  flex flex-col md:flex-row  md:h-[500px] outline `}>
+        <Card className={`flex flex-col gap-5 justify-center  `}>
+                <Container className={`md:w-[400px] pt-6 pb-10 bg-gradient-to-t from-royal to-slate flex flex-col items-center rounded-b-[30px] md:rounded-[30px] gap-5`}>
                     <Title className={`text-lavender text-[21px] md:text-[24px] font-medium`}>Your Result</Title>
                     <Container 
                     className={`bg-gradient-to-b from-violet to-persian text-pale w-48 h-48 md:w-[220px] md:h-[220px] flex flex-col items-center justify-center rounded-full text-[64px] font-bold leading-snug`}>
@@ -59,21 +61,21 @@ const Cards = () => {
                       <Paragraph className={`text-lavender`}>You scored higher than 65% of <br className={`md:block hidden`} /> the <br className={`md:hidden`} /> people who have taken <br className={`md:block hidden`} /> these tests.</Paragraph>
                     </Container>
                 </Container>
-            <Card className={`bg-white flex flex-col items-center`}>
-                <Container className={`w-full outline`}>
+        </Card>
+            <Card className={`bg-white w-full flex flex-col items-center gap-7`}>
+                <Container className={`px-10 w-full outline`}>
                         <Container>
                           <Title className={`text-[21px] font-medium`}>Summary</Title>
                         </Container>
-                        <ul>
+                        <ul className={`flex flex-col gap-5`}>
                           {list && list.map((item, index) => <li key={index} 
-                          className={`text-${handleSummaryTextColor(index)} bg-${handleSummaryTextColor(index)} bg-opacity-10 flex outline`}>
+                          className={`text-${handleSummaryTextColor(index)} bg-${handleSummaryTextColor(index)} ${index === 0 ? 'hidden' : ''} bg-opacity-10 flex p-4 rounded-xl flex-wrap outline`}> {/* fix the alignment */}
                           <img src={`public/images/icon-${handleIcons(index)}.svg`} alt="" />{item.category} <span className={'flex justify-end w-full gap-1'}>{index ? item.score : ''} <span>{index ? '/ 100' : ''}</span></span></li>)}
                         </ul>
-                        <Container className={`flex justify-center`}><Button className={`outline w-40`}>Continue</Button></Container>
                 </Container>
+                <Container className={`flex justify-center`}><Button className={`outline w-40`}>Continue</Button></Container>
 
             </Card>
-        </Card>
     </Container>
   )
 }
